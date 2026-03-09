4c1b453a77424ff7b157dae59b9808a1b0c9dcbf
Prem__brnwal <premburnwal31@gmail.com>
2026-03-09 22:42:29 +0530

    stablishing of the CLI project

---
+++ Web-Scraper-CLI/src/main/java/com/example/Web/Scraper/CLI/WebScraperCliApplication.java
@@ -0,0 +1,13 @@
+package com.example.Web.Scraper.CLI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebScraperCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebScraperCliApplication.class, args);
+	}
+
+}
---
+++ Web-Scraper-CLI/src/test/java/com/example/Web/Scraper/CLI/WebScraperCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Web.Scraper.CLI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebScraperCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
